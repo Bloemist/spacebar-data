f64714f314fc8be83f1a977bb9b8d309d3822f8f
hsg-spacebar <spacebar@kwitz.net>
2016-10-22 00:26:09 +0200

    test

--- gitpush_example.php
+++ gitpush_example.php
@@ -1,4 +1,8 @@
 <?php
+$output = shell_exec('git config --global user.email "spacebar@kwitz.net" 2>&1');
+echo $output;
+$output = shell_exec('git config --global user.name "hsg-spacebar" 2>&1');
+echo $output;
 $output = shell_exec('git commit -am "new transaction" 2>&1');
 echo $output;
 $output = shell_exec('git push https://hsg-spacebar:password@github.com/Bloemist/spacebar master 2>&1');
